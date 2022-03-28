5cbd15e57acfb03638024b22c4940b282e3cdf57
PrinceIsac <luhangaprinceisac@gmail.com>
2022-03-28 23:52:09 +0200

    Task 0.4

---
+++ FourthTask.java
@@ -0,0 +1,13 @@
+public class FourthTask {
+    static void evenOrOdd(int number){
+        if(number % 2 == 0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+   
+    public static void main(String[] args) {
+        evenOrOdd(9);
+    }
+}
