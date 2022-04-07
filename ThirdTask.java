c8821cc6e12c55f7e0d6fbc5d456fd1755b73853
PrinceIsac <luhangaprinceisac@gmail.com>
2022-04-07 23:15:39 +0200

    Task 0.3

---
+++ ThirdTask.java
@@ -0,0 +1,10 @@
+public class ThirdTask {
+    static void hello(String name){
+        String greetings = "Hello ";
+        System.out.println(greetings + name + '!') ;
+    }
+  
+    public static void main(String[] args) {
+        hello("Tshepo");
+    }
+}
