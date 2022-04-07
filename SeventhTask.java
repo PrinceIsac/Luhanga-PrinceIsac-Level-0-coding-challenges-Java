87d98b6046618963b2c4e738e07ae502b0b4ada4
PrinceIsac <luhangaprinceisac@gmail.com>
2022-04-07 23:16:43 +0200

    Task 0.7

---
+++ SeventhTask.java
@@ -0,0 +1,17 @@
+public class SeventhTask {
+  public static double celsiusToFahrenheit(double temperature){
+        double fahrenheit = ((temperature * 9/5) + 32);
+        return fahrenheit;
+   }
+
+  public static double fahrenheitToCelsius(double temperature){
+        double celsius = (temperature - 32) * 5/9;
+        return celsius;
+    }
+  
+    public static void main(String[] args) {
+         celsiusToFahrenheit(25);
+         fahrenheitToCelsius(104);
+    }
+}
+
