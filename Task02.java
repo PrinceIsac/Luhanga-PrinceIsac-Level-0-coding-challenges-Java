ac24736ce78fd679f008405eb22c98ac695acae4
PrinceIsac <luhangaprinceisac@gmail.com>
2022-04-09 14:44:44 +0200

    Changed data type of b

--- Task02.java
+++ Task02.java
@@ -1,11 +1,12 @@
-public class SecondTask {
+public class Task02 {
     public static void main(String[] args) {
-        int x =1 + 1 * 2;
+        int x = 1 + 1 * 2;
         int y = (1 + 1) * 2;
         int z = 1 + ( 1 * 2 );
         int a = 1 + 1 * 2 / 2;
-        int b = (1 + 1 * 2 ) /  2;
+        double b = (1 + 1 * 2) /  2;
 
         System.out.println("x = " + x + "\n"+ "y = " + y + "\n" + "z = " + z + "\n" + "a = " + a + "\n" + "b = " + b);
     }
 }
+
