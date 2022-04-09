ccc7a2e09945863bf9b26a774da00e974c322ad7
PrinceIsac <luhangaprinceisac@gmail.com>
2022-04-09 14:45:49 +0200

    Calculated Area of triangle correctly

--- Task05.java
+++ Task05.java
@@ -1,14 +1,14 @@
-public class FifthTask {
+public class Task05 {
    static double areaOfTriangle(double side1, double side2, double side3){
-       double area = 0.0;
-       double sideSum = (side1 + side2 + side3);
-       area = sideSum / 2;
+      double area = 0.0;
+      double halfPerimeter = (side1 + side2 + side3) / 2;
+      area = Math.sqrt(halfPerimeter * (halfPerimeter - side1) * (halfPerimeter - side2) * (halfPerimeter - side3));
       
-    return area;
+      return area;
    }
    
    public static void main(String[] args) {
-      areaOfTriangle(10, 20, 2);
+      areaOfTriangle(10, 10, 10);
    }
 }
 
