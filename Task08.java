b3d6d211298cba4739b81bab9d91512faa9cae86
PrinceIsac <luhangaprinceisac@gmail.com>
2022-04-21 11:58:29 +0200

    corrected camel case of numberOfHours

--- Task08.java
+++ Task08.java
@@ -1,35 +1,22 @@
-public class EighthTask {
+public class Task08 {
     public static void convertNumberToTime(int number){
-        int numberOfhours = number / 60;
-        int numberOfminutes = number % 60;
-        String hour = " hour, ";
-        String hours = " hours, ";
-        String minute = " minute";
-        String minutes = " minutes";
-
-         if(numberOfhours == 1 && numberOfminutes == 1){
-            System.out.println(numberOfhours + hour + numberOfminutes + minute);
+        int numberOfHours = number / 60;
+        int numberOfMinutes = number % 60;
+        String hours = " hour";
+        String minutes = " minute";
+        String result = "";
+        
+        if(numberOfHours == 0 || numberOfHours > 1){
+            hours += "s";
         }
-         else if(numberOfhours == 1 && (numberOfminutes > 1 || numberOfminutes == 0)){
-            System.out.println(numberOfhours + hour + numberOfminutes + minutes);
-        } 
-         else if(numberOfhours > 1 && numberOfminutes == 1){
-            System.out.println(numberOfhours + hours + numberOfminutes + minute);
+        if(numberOfMinutes == 0 || numberOfMinutes > 1){
+            minutes += "s";
         }
-        else if(numberOfhours > 1 && (numberOfminutes > 1 || numberOfminutes == 0)){
-            System.out.println(numberOfhours + hours + numberOfminutes + minutes);
-        }else if(numberOfhours == 0 &&  numberOfminutes == 1){
-            System.out.println(numberOfhours + hours + numberOfminutes + minute);
-        }
-        else if(numberOfhours == 0 &&  numberOfminutes > 1){
-            System.out.println(numberOfhours + hours + numberOfminutes + minutes);
-        }else if(numberOfhours == 0 && numberOfminutes == 0){
-            System.out.println(numberOfhours + hours + numberOfminutes + minutes);
-        }
+        result += numberOfHours + hours + ", " + numberOfMinutes + minutes;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-        convertNumberToTime(63);
+        convertNumberToTime(2041);
     }
 }
-
