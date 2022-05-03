b10c45b0e109286937e797917f9c5407bf323d9f
PrinceIsac <luhangaprinceisac@gmail.com>
2022-05-04 00:10:54 +0200

    Changed class name

--- Task01.java
+++ Task01.java
@@ -1,11 +1,11 @@
-public class FirstTask{
+public class Task01{
     public static void main(String[] args) {
         int x = 0;
         int y = 1;
         System.out.println(x);
         System.out.println(y);
         x = x + 3;
-        y = y + 3;
+        y = y + x;
         System.out.println(x);
         System.out.println(y);
 
